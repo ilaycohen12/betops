e0624e48b3cc8086199240c7a0d1d1749a38ad3b
ilaycohen12 <ilaycohen456@gmail.com>
2026-06-02 17:06:54 +0300

    testing a new message 3-html

--- lambdas/api/handler.py
+++ lambdas/api/handler.py
@@ -1,9 +1,16 @@
-import json
-
-
 def lambda_handler(event, context):
+    html = """
+<!DOCTYPE html>
+<html>
+<head><title>BetOps</title></head>
+<body>
+  <h1>Welcome to BetOps 🎲</h1>
+  <p>Your friends betting platform is coming soon.</p>
+</body>
+</html>
+"""
     return {
         "statusCode": 200,
-        "headers": {"Content-Type": "application/json"},
-        "body": json.dumps({"message": "this is betops!!!"}),
+        "headers": {"Content-Type": "text/html"},
+        "body": html,
     }
