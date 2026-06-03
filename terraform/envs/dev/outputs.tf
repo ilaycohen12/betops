509e58ee6110f042733f9f30476fec1c93ccb830
ilay <ilaycohen456@gmail.com>
2026-06-03 18:37:40 +0300

    feat: add dev terraform environment

---
+++ terraform/envs/dev/outputs.tf
@@ -0,0 +1,14 @@
+output "api_url" {
+  description = "Public URL of the dev API"
+  value       = aws_apigatewayv2_stage.default.invoke_url
+}
+
+output "db_endpoint" {
+  description = "RDS Postgres endpoint"
+  value       = module.rds.db_endpoint
+}
+
+output "db_secret_arn" {
+  description = "ARN of DB credentials in Secrets Manager"
+  value       = module.rds.db_secret_arn
+}
